15a11fef895c36e0b5309abded4e48a3547b120c
chotelal patel <chhotupatel705@gmail.com>
2024-02-25 22:40:00 +0530

    update

--- server/index.js
+++ server/index.js
@@ -3,12 +3,6 @@ const cors=require("cors");
 const mongoose=require("mongoose");
 const userRoutes=require("./routes/userRoutes");
 const messageRoute=require("./routes/messagesRoute");
-//niche wala new lagaya hu
-
-
-const userrotes=require('./model/userModel');
-const messageroute=require('./model/messageModel');
-
 
 //niche wala new lagaya hu
 const userrotes=require('./model/userModel');
@@ -28,36 +22,6 @@ app.use(express.json());
 app.use("/api/auth",userRoutes);
 app.use("/api/messages",messageRoute);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a35024b5f46db1eec20e5da4d34149fdf5852124
-// mongoose.connect(process.env.MONGO_URL,{
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true,
-// })
-// .then(()=>{
-//     console.log("mongoDB connect Sucessfull");
-<<<<<<< HEAD
-//     console.log(process.env.MONGO_URL);
-=======
->>>>>>> a35024b5f46db1eec20e5da4d34149fdf5852124
-// })
-// .catch((err)=>{
-//     console.log(err.message);
-// });
-
-<<<<<<< HEAD
-
-
-=======
-// const server=app.listen(process.env.PORT,()=>{
-//    console.log(`Server Started on Port ${process.env.PORT}`);
-// });
-
-
-//ye new add kiya gaya hai
->>>>>>> a35024b5f46db1eec20e5da4d34149fdf5852124
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
