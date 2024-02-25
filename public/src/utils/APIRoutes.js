16040d2a99920b562a8ed2f0bc5e2a64a0eda15c
chotelal patel <chhotupatel705@gmail.com>
2024-02-25 23:48:38 +0530

    update

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,5 +1,6 @@
 // export const host="https://messanger-v1ql.onrender.com";
-export const host = "http://127.0.0.1:5000";
+export const host ="https://messanger-v1ql.onrender.com"
+//export const host = "http://127.0.0.1:5000";
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`;
 export const setAvatarRoute =`${host}/api/auth/setAvatar`;
